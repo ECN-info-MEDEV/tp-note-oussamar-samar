b367fdbd22e1c6348d65d4f89396f16bea1617d6
samarelayed <elayedsamar@gmail.com>
2023-01-26 08:46:45 +0100

    test1

---
+++ TP-note-ELBETTAL-ELAYED/src/main/java/com/mycompany/tp/note/oussamar/samar/Codeur.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp.note.oussamar.samar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author samar
+ */
+public class Codeur {
+    public void choix(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("choisit 4 pions: J pour Jaune, R pour Rouge, B pour Bleu, V pour Vert");
+        String str = sc.nextLine();
+        if ( ){
+            
+        }
+    }
+}
--- TP-note-ELBETTAL-ELAYED/src/main/java/com/mycompany/tp/note/oussamar/samar/Pion.java
+++ TP-note-ELBETTAL-ELAYED/src/main/java/com/mycompany/tp/note/oussamar/samar/Pion.java
@@ -4,10 +4,27 @@
  */
 package com.mycompany.tp.note.oussamar.samar;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author samar
  */
 public class Pion {
+    ArrayList <String> ListCouleur=new ArrayList<>();
+    String couleur;
+
+    public Pion() {
+        ListCouleur.add("J");
+        ListCouleur.add("R");
+        ListCouleur.add("B");
+        ListCouleur.add("V");
+        ListCouleur.add("B");
+        ListCouleur.add("N");
+        
+    }
+    public Pion(String couleur){
+        this.couleur=couleur;
+    }
     
 }
