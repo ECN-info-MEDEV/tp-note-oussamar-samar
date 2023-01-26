71764df83f862e6b79ad5cc5973815863ac47b56
ELBETTALO <eb.oussama3@gmail.com>
2023-01-26 09:00:59 +0100

    Creation de quelque methodes dans la classe PlateauJeu

--- TP-note-ELBETTAL-ELAYED/src/main/java/com/mycompany/tp/note/oussamar/samar/PlateauJeu.java
+++ TP-note-ELBETTAL-ELAYED/src/main/java/com/mycompany/tp/note/oussamar/samar/PlateauJeu.java
@@ -9,5 +9,31 @@ package com.mycompany.tp.note.oussamar.samar;
  * @author samar
  */
 public class PlateauJeu {
+
+    private String[][] plateau ;
+    
+    
+    
+    
+    public PlateauJeu() {
+        plateau = new String[12][4];
+    }
+    
+    public void setColor(int row, int col, String couleur) {
+        plateau[row][col] = couleur ;
+    }
+
+    public String getColor(int row, int col) {
+        return plateau[row][col];
+    }
+    
+    
+    public void Manche(){
+         
+        
+        
+    
+    
+    }
     
 }
